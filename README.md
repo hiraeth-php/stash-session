e0d5f0abbd89a77019990113f0802ddb20859087
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-04-13 13:24:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This package provides cached based session storage via Stash for the Hiraeth Nano Framework
+
+
+## Environment
+
+```toml
+[SESSION]
+
+	TTL = 3600 ; 1 hour sessions, default is 2 hours
+```
---
+++ src/delegates/HandlerDelegate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hiraeth\Stash\Session;
+
+use Hiraeth;
+use Cache\SessionHandler\Psr6SessionHandler;
+
+/**
+ *
+ */
+class HandlerDelegate implements Hiraeth\Delegate
+{
+	/**
+	 * Get the class for which the delegate operates.
+	 *
+	 * @static
+	 * @access public
+	 * @return string The class for which the delegate operates
+	 */
+	static public function getClass(): string
+	{
+		return Psr6SessionHandler::class;
+	}
+
+
+	/**
+	 * Get the instance of the class for which the delegate operates.
+	 *
+	 * @access public
+	 * @param Hiraeth\Application $app The application instance for which the delegate operates
+	 * @return object The instance of the class for which the delegate operates
+	 */
+	public function __invoke(Hiraeth\Application $app): object
+	{
+		$manager = $app->get(Hiraeth\Caching\PoolManager::class);
+		$pool    = $manager->get('session');
+
+		return new Psr6SessionHandler($pool, [
+			'ttl'    => $app->getEnvironment('SESSION.TTL', 60 * 60 * 2),
+			'prefix' => 'sessions/'
+		]);
+	}
+}
